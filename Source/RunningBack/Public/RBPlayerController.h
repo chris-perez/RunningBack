ed93a4c31df1926552c00b1f888263f9889f4a1a
Chris <chrisv.perez@gmail.com>
2016-05-01 21:24:02 -0400

    fixed kill box to respawn on kill

---
+++ Source/RunningBack/Public/RBPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "RBPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNNINGBACK_API ARBPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
