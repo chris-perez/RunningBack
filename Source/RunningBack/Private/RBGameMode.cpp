abeb7f2110aa64dae7b28fcd08cef84034eaa3d5
Chris <chrisv.perez@gmail.com>
2016-09-14 12:03:34 -0400

    starting converting blueprints to cpp

---
+++ Source/RunningBack/Private/RBGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RunningBack.h"
+#include "RBGameMode.h"
+
+AActor* ARBGameMode::ChoosePlayerStart_Implementation(AController* player) {
+	APlayerController* playerController =  static_cast<APlayerController*>(player);
+	
+}
+
+void ARBGameMode::GetSpawn(AController* Player) {
+
+}
